b74152b9f0caf6e99dbbd8e51e7ff00cdb40105d
Ali Hejazizo <hejazizo@ualberta.ca>
2018-02-27 00:34:46 -0700

    removed DB-main

--- bot_token.py
+++ bot_token.py
@@ -1,5 +1,5 @@
 import telebot
-token = '426758191:AAH77L8fIWpCgc68tJ_WzWmBm-lgXsTiIpw'
+token = 'token'
 print('Bot started with token: {}'.format(token))
 
 bot = telebot.TeleBot(token= token, threaded=False)
