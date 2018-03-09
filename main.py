9e8d46e502b20599305478494e4e8fea2799e3d6
Ali Hejazizo <hejazizo@ualberta.ca>
2018-03-09 03:41:20 -0700

    users info are now updated by each query

--- main.py
+++ main.py
@@ -139,6 +139,12 @@ def chat(message):
                                          (message.from_user.id,)).fetchone()
         if state is not None:
             state = state[0]
+
+            cur.execute('''UPDATE Users SET first_name = (%s), last_name = (%s), username = (%s), WHERE tel_id = (%s)''',
+                        (message.from_user.first_name,
+                         message.from_user.last_name,
+                         message.from_user.username,
+                         message.from_user.id))
             ####################################
             ####            IDLE            ####
             ####################################
